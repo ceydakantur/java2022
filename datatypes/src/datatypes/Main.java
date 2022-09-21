de3c477db01a209a7d49d2ee2722ae62d1b7e2b1
ceydakantur <ceydakantur@localhost>
2022-09-21 21:16:23 +0300

    datatypes

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,21 @@
+package datatypes;
+
+public class Main{
+
+	public static void main(String[] args) {
+		int sayı= 12;
+		sayı = 13;
+		double ondalık = 23.2;
+		
+		char karakter = 'A';
+		String şehir = "Ankara";
+		boolean doğruMu = false;
+		if (doğruMu== true) 
+		{
+			
+		}
+		
+		
+	}
+
+}
